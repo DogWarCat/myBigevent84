7537d2c1b016376c9cca1ceebb21d80a3ce33a5f
DogWarCat <526659053@qq.com>
2019-09-23 15:35:15 +0800

    添加文章类别的url config

--- web_back/js/utils/config_副本.js
+++ web_back/js/utils/config_副本.js
@@ -2,5 +2,10 @@ var BASE_URL='http://localhost:8000';
 var URL_LIST={
 	user_login:BASE_URL+'/admin/login',
 	user_logout:BASE_URL+'/admin/logout',
-	user_getuser:BASE_URL+'/admin/getuser'
+	user_getuser:BASE_URL+'/admin/getuser',
+	category_search:BASE_URL+'/admin/category_search',
+	category_add:BASE_URL+'/admin/category_add',
+	category_edit:BASE_URL+'/admin/category_edit',
+	category_delete:BASE_URL+'/admin/category_delete',
+	comment_search:BASE_URL+'/admin/comment_search'
 }
